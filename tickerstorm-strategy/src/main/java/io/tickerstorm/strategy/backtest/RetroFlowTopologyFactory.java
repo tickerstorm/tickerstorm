b4ca0b2140690980d85657fdc70c01c7d1a67002
Krzysztof Karski <kkarski@pixtulate.com>
2016-06-17 10:07:00 -0700

    added model bolt

--- tickerstorm-strategy/src/main/java/io/tickerstorm/strategy/backtest/RetroFlowTopologyFactory.java
+++ tickerstorm-strategy/src/main/java/io/tickerstorm/strategy/backtest/RetroFlowTopologyFactory.java
@@ -3,7 +3,9 @@ package io.tickerstorm.strategy.backtest;
 import javax.annotation.PostConstruct;
 
 import org.apache.storm.generated.StormTopology;
+import org.apache.storm.jms.bolt.JmsBolt;
 import org.apache.storm.jms.spout.JmsSpout;
+import org.apache.storm.topology.BoltDeclarer;
 import org.apache.storm.topology.TopologyBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -22,14 +24,33 @@ public class RetroFlowTopologyFactory {
   @Autowired
   private JmsSpout commandsSpout;
 
+  @Qualifier("modelData")
+  @Autowired
+  private JmsBolt modelDataBolt;
+
   private final TopologyBuilder retroBuilder = new TopologyBuilder();
 
   protected RetroFlowTopologyFactory() {}
+
+  private boolean modelData;
 
   @PostConstruct
   private void init() {
     retroBuilder.setSpout("retroModelData", retroModelData);
     retroBuilder.setSpout("commands", commandsSpout);
+  }
+
+  public RetroFlowTopologyFactory storeToModelData() {
+
+    if (modelData)
+      return this;
+
+    //Should not stream back model events since they are already stored. Only store new generated events
+    //BoltDeclarer dec = retroBuilder.setBolt("modelData", modelDataBolt).localOrShuffleGrouping("retroModelData");
+
+    modelData = true;
+
+    return this;
   }
 
   public StormTopology build() {
