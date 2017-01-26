58ecc7c0942f4226b41e82d4c8dd9e0727ed78c3
Krzysztof Karski <kkarski@pixtulate.com>
2017-01-25 20:54:22 -0800

    refactored jms connection settings

--- tickerstorm-data/src/test/java/io/tickerstorm/data/TestMarketDataServiceConfig.java
+++ tickerstorm-data/src/test/java/io/tickerstorm/data/TestMarketDataServiceConfig.java
@@ -1,26 +1,57 @@
+/*
+ * Copyright (c) 2017, Tickerstorm and/or its affiliates. All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     - Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     - Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ *     - Neither the name of Tickerstorm or the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *   THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *   PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 package io.tickerstorm.data;
 
+import com.google.common.base.Throwables;
+import io.tickerstorm.common.eventbus.ByDestinationNameJmsResolver;
+import io.tickerstorm.common.eventbus.Destinations;
 import javax.jms.Session;
-
+import org.apache.activemq.jms.pool.PooledConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
 import org.springframework.util.ErrorHandler;
 
-import com.google.common.base.Throwables;
-
-import io.tickerstorm.common.eventbus.ByDestinationNameJmsResolver;
-import io.tickerstorm.common.eventbus.Destinations;
-
-@EnableAutoConfiguration
-@Configuration
+@SpringBootApplication
 public class TestMarketDataServiceConfig extends MarketDataApplication {
+
+  @Value("${service.name}")
+  protected String serviceName;
 
   private final static Logger logger = LoggerFactory.getLogger(TestMarketDataServiceConfig.class);
 
@@ -30,27 +61,25 @@ public class TestMarketDataServiceConfig extends MarketDataApplication {
 
   @Qualifier("realtime")
   @Bean
-  public DefaultMessageListenerContainer buildQueryListenerContainer(CachingConnectionFactory factory) {
+  public DefaultMessageListenerContainer buildQueryListenerContainer(
+      PooledConnectionFactory connectionFactory) {
 
-    DefaultMessageListenerContainer container = null;
+    DefaultMessageListenerContainer container = new DefaultMessageListenerContainer();
+    container.setConnectionFactory(connectionFactory);
+    container.setDestinationName(
+        "Consumer." + serviceName + "." + Destinations.TOPIC_REALTIME_MARKETDATA);
+    container.setSessionAcknowledgeMode(Session.AUTO_ACKNOWLEDGE);
+    container.setDestinationResolver(new ByDestinationNameJmsResolver());
+    container.setMaxMessagesPerTask(-1);
+    container.setAutoStartup(false);
+    container.setErrorHandler(new ErrorHandler() {
 
-    try {
-      container = new DefaultMessageListenerContainer();
-      container.setConnectionFactory(factory);
-      container.setDestinationName(Destinations.TOPIC_REALTIME_MARKETDATA);
-      container.setSessionAcknowledgeMode(Session.AUTO_ACKNOWLEDGE);
-      container.setDestinationResolver(new ByDestinationNameJmsResolver());
-      container.setMaxMessagesPerTask(-1);
-      container.setErrorHandler(new ErrorHandler() {
+      @Override
+      public void handleError(Throwable t) {
+        logger.error(Throwables.getRootCause(t).getMessage(), t);
+      }
+    });
 
-        @Override
-        public void handleError(Throwable t) {
-          logger.error(Throwables.getRootCause(t).getMessage(), t);
-        }
-      });
-    } catch (Exception e) {
-      logger.error(Throwables.getRootCause(e).getMessage(), e);
-    }
     return container;
   }
 }
